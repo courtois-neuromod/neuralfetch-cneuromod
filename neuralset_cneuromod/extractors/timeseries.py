6c868ac47786b6bbef9c4f01d34d44f8bcadabed
MarieStLaurent <marie.stl@gmail.com>
2026-08-04 18:14:41 -0400

    tseries extract

--- neuralset_cneuromod/extractors/timeseries.py
+++ neuralset_cneuromod/extractors/timeseries.py
@@ -63,11 +63,11 @@ class TimeseriesExtractor(BaseExtractor):
     frequency: tp.Literal["native"] | float = "native"
 
     query: base.Query | None = None
-    infra: MapInfra = MapInfra(
-        timeout_min=120,
-        cpus_per_task=10,
-        version="2",
-    )
+    #infra: MapInfra = MapInfra(
+    #    timeout_min=120,
+    #    cpus_per_task=10,
+    #    version="2",
+    #)
 
     def _preprocess_event(self, event: Timeseries) -> TimedArray:
         """"""
